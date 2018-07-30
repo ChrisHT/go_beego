9b39916de5d79c5ad0282d655aac9a1588efb455
ChrisHT <lingxun1994@gmail.com>
2018-07-31 01:52:30 +0800

    update

--- controllers/user/user.go
+++ controllers/user/user.go
@@ -14,6 +14,7 @@ type UserController struct {
 func (this *UserController) Login() {
 	name := this.GetString("name")
 	password := this.GetString("password")
+	fmt.Println(name, password)
 
 	if name == "" {
 		this.Data["json"] = map[string]interface{}{"code": 1, "message": "name错误"}
@@ -30,11 +31,17 @@ func (this *UserController) Login() {
 	user := userModel.User{}
 
 	if code, err := user.FindByName(name); code != 0 || err != nil {
-		this.Data["json"] = map[string]interface{}{"code": code, "message": err}
+		this.Data["json"] = map[string]interface{}{"code": code, "message": "user未注册"}
 		this.ServeJSON()
 		return
 	}
 	fmt.Println(user)
+
+	if ok, err := user.CheckPass(password); ok != true || err != nil {
+		this.Data["json"] = map[string]interface{}{"code": code, "message": "password错误"}
+		this.ServeJSON()
+		return
+	}
 
 	if user.Password != password {
 		this.Data["json"] = map[string]interface{}{"code": 1, "message": "password错误"}
@@ -42,5 +49,47 @@ func (this *UserController) Login() {
 		return
 	}
 	this.Data["json"] = map[string]interface{}{"code": 0, "message": "登录成功"}
+	this.ServeJSON()
+}
+
+func (this *UserController) Register() {
+	name := this.GetString("name")
+	password := this.GetString("password")
+	fmt.Println(name, password)
+
+	if name == "" {
+		this.Data["json"] = map[string]interface{}{"code": 1, "message": "name错误"}
+		this.ServeJSON()
+		return
+	}
+
+	if password == "" {
+		this.Data["json"] = map[string]interface{}{"code": 1, "message": "password错误"}
+		this.ServeJSON()
+		return
+	}
+
+	user := userModel.User{}
+
+	if code, err := user.FindByName(name);  err = nil {
+		this.Data["json"] = map[string]interface{}{"code": code, "message": "user已注册"}
+		this.ServeJSON()
+		return
+	}
+
+	regDate := time.Now()
+	_user, err := models.NewUser(&user, regDate)
+	if err != nil {
+		this.Data["json"] = map[string]interface{}{"code": code, "message": err}
+		this.ServeJSON()
+		return
+	}
+	if code, err := _user.Insert(); err != nil {
+		this.Data["json"] = map[string]interface{}{"code": code, "message": "注册失败"}
+		this.ServeJSON()
+		return
+	}
+
+	this.Data["json"] = map[string]interface{}{"code": code, "message": "success"}
 	this.ServeJSON()
 }
--- models/user/user.go
+++ models/user/user.go
@@ -4,14 +4,27 @@ import (
 	"go_beego/models/mongodb"
 	"gopkg.in/mgo.v2/bson"
 	"fmt"
+	"io"
+	"golang.org/x/crypto/scrypt"
 )
 
 type User struct {
 	//ID       string    `bson:"_id"      json:"_id,omitempty"`
 	Name     string    `bson:"name"     json:"name,omitempty"`
 	Password string    `bson:"password" json:"password,omitempty"`
-	//Salt     string    `bson:"salt"     json:"salt,omitempty"`
-	//Date     time.Time `bson:"date"     json:"date,omitempty"`
+	Salt     string    `bson:"salt"     json:"salt,omitempty"`
+	Date     time.Time `bson:"date"     json:"date,omitempty"`
+}
+
+const pwHashBytes = 64
+
+func generateSalt() (salt string, err error) {
+	buf := make([]byte, pwHashBytes)
+	if _, err := io.ReadFull(rand.Reader, buf); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", buf), nil
 }
 
 func (u *User) FindByName(name string) (code int, err error) {
@@ -28,4 +41,54 @@ func (u *User) FindByName(name string) (code int, err error) {
 		code = 0
 	}
 	return
+}
+
+func NewUser(u *User, t time.Time) (u *User, err error) {
+	salt, err := generateSalt()
+	if err != nil {
+		return nil, err
+	}
+	hash, err := generatePassHash(u.Password, salt)
+	if err != nil {
+		return nil, err
+	}
+
+	user := User{
+		Name:     u.Name,
+		Password: hash,
+		Salt:     salt,
+		Date:  t}
+
+	return &user, nil
+}
+
+func (u *User) InSert() (code int, err error) {
+	mConn := mymongo.Conn()
+	defer mConn.Close()
+
+	c := mConn.DB("test").C("user")
+	err = c.Insert(u)
+
+	if err != nil {
+		code = 1
+	} else {
+		code = 0
+	}
+	return
+}
+
+func generatePassHash(password string, salt string) (hash string, err error) {
+	h, err := scrypt.Key([]byte(password), []byte(salt), 16384, 8, 1, pwHashBytes)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", h), nil
+}
+
+func (u *User) CheckPass(password string) (ok bool, err error) {
+	hash, err := generatePassHash(password, u.Salt)
+	if err != nil {
+		return false, err
+	}
+	return u.Password == hash, nil
 }
